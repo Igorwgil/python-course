77f2b95117a8650a98d48eb9c75f20b4a3bf7e17
Igor Daniel <igordaniel.wgil6@gmail.com>
2025-05-13 15:45:20 -0400

    Update name-list.py

--- name-list.py
+++ name-list.py
@@ -4,5 +4,5 @@ nameList = [0] * end
 for i in range(end):
     nameList[i] = input('White the name: ')
 
-for i in range(end - 1, -1, -1):
+for i in range(end - 1, -1, -1): 
     print('i', i, '-', nameList[i])
